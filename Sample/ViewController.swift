426cb383eaf2dd6f988a421b9ad049dd8a288b71
Alina Hambaryan <alina_hambaryan@yahoo.com>
2017-05-02 10:31:09 +0200

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by ALINA HAMBARYAN on 5/2/17.
+//  Copyright © 2017 ALINA HAMBARYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
